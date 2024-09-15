edb79c05050365062fcc8b249581e3ed2ee5364f
gopaltiwari04 <142602636+gopaltiwari04@users.noreply.github.com>
2024-09-15 05:57:48 +0530

    Create User.java

---
+++ src/basic/User.java
@@ -0,0 +1,26 @@
+package basic;
+
+public class User {  // Removed 'public'
+    protected String name;
+    protected String email;
+    protected String password;
+
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User() {
+
+    }
+
+
+    public void viewOptions() {
+
+    }
+}
+
+
+
+
