9db1187c4e523a92792b238821f3772c375de9e8
gopaltiwari04 <142602636+gopaltiwari04@users.noreply.github.com>
2024-09-15 05:54:56 +0530

    Create Complaint.java

---
+++ src/basic/Complaint.java
@@ -0,0 +1,24 @@
+package basic;
+
+public class Complaint {
+    private String description;
+    private String status;
+
+    public Complaint(String description, String status) {
+        this.description = description;
+        this.status = status;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
+
